1d119239def40cb8a9398c802384e8ed27aaef60
Jun-Mo <yunedoopea610@gmail.com>
2017-12-17 05:29:58 +0900

    Modify style error

--- Server/ServerCode/static/script/script.js
+++ Server/ServerCode/static/script/script.js
@@ -1,9 +1,47 @@
 window.onload = function() {
-//    let inputFile = document.querySelectorAll("input[type='file']");
-//    let imgCheck = document.getElementsByClassName("imgCheck");
-//    for(let i =0; i<inputFile.length; i++) {
-//        inputFile[i].onchange = function() {
-//            imgCheck[i].style.background = "url("+");";
-//        }
-//    }
+    let inputFile = document.querySelectorAll("input[type='file']");
+    let imgCheck = document.getElementsByClassName("imgCheck");
+    for(let i in inputFile) {
+        inputFile[i].onchange = function(e) {
+            e.preventDefault();
+            let appendImg = document.querySelector('article');//willChange
+
+            let file = uploadImg.files[0],
+                reader = new FileReader();
+
+            reader.onload = function (event){
+                let img = new Image();
+                img.src = event.target.result;
+
+                appendImg.innerHTML = '';
+                appendImg.appendChild(img);
+                appendImg.appendChild(createDiv);
+            };
+            reader.readAsDataURL(file);
+            return false;
+        }
+    }
+    
+    let uploadImg = document.getElementById('backPic');
+            let createDiv = document.createElement('div');
+            createDiv.setAttribute('id', 'blackCover');
+            
+            uploadImg.onchange = function (e) {
+                e.preventDefault();
+                let appendImg = document.querySelector('article');//willChange
+                
+                let file = uploadImg.files[0],
+                    reader = new FileReader();
+                
+                reader.onload = function (event){
+                    let img = new Image();
+                    img.src = event.target.result;
+                    
+                    appendImg.innerHTML = '';
+                    appendImg.appendChild(img);
+                    appendImg.appendChild(createDiv);
+                };
+                reader.readAsDataURL(file);
+                return false;
+            };
 };
