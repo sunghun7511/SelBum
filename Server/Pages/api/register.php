cce18bfddeda629468b585cb47475705f56e23a8
SHGroup <sunghun7511@naver.com>
2017-12-17 00:47:32 +0900

    Add create.php & Modify register.php

--- Server/Pages/api/register.php
+++ Server/Pages/api/register.php
@@ -5,6 +5,8 @@
     $nickname = $_POST['nickname'];
     $email = $_POST['email'];
 
+    $checkEmail = filter_var($email, FILTER_VARIDATE_EMAIL);
+
     $queryLogin = "select * from Users where username='{$username}' and password='{$password}'";
     $queryLoginResult = mysqli_query($sql_conn, $queryLogin);
     $queryLoginArray = mysqli_fetch_array($queryLoginResult);
@@ -15,21 +17,25 @@
     }
 
     if(isset($queryLoginArray['username']))
-        d400("");
+        d400("already defined username");
     
     if(!isset($username))
-        d400("");
+        d400("username not found");
+
+    if(!isset($password))
+        d400("password not found");
+
+    if(!isset($nickname))
+        d400("nickname not found");
+
+    if(!isset($email)) 
+        d400("email not found");
     
-    if(!isset($password)){
+    if($checkEmail == FALSE)
+        d400("synatx error email");
 
-    }
-    if(!isset($nickname)){
-
-    }
-    if(!isset($email)) {
-    }
     $queryRegister = "insert into ('username', 'password', 'nickname', 'email') values ('{$username}', '{$password}', '{$nickname}', '{$email}')";
     mysqli_query($sql_conn, $queryRegister);
     http_response_code(200);
-    die("");
+    die(generate_AccessToken());
 ?>
