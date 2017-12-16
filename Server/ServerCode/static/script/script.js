21570d1403c37d1c731c869ebc3de665db4c7dae
Jun-Mo <yunedoopea610@gmail.com>
2017-12-17 03:45:22 +0900

    templates/personalForm : finish style

--- Server/ServerCode/static/script/script.js
+++ Server/ServerCode/static/script/script.js
@@ -1,70 +1,9 @@
 window.onload = function() {
-    let loginSubmit = document.getElementById("loginSubmit");
-    if(loginSubmit != null){
-        submit.onclick = function () {
-            var xhr = new XMLHttpRequest();
-            
-            username = document.getElementById("loginId").value;
-            password = document.getElementById("loginPw").value;
-            
-            
-            xhr.onreadystatechange = function() {
-                if(this.readyState == 4){
-                    if(this.status == 200){
-                        alert("Success! " + this.responseText);
-                    }else if(this.status == 400){
-                        alert("Fail! " + this.responseText);
-                    }else{
-                        alert(this.status + this.responseText);
-                    }
-                }
-            };
-            params = "username=" + username + "&password=" + password;
-            
-            console.log(params);
-            
-            xhr.open("POST", "http://lapio.kr/api/login.php", true);
-            
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            
-            xhr.send(params);
-
-            return false;
-        }
-    }
-
-    let signupSubmit = document.getElementById("signupSubmit");
-    if(signupSubmit != null){
-        submit.onclick = function () {
-            var xhr = new XMLHttpRequest();
-            
-            username = document.getElementById("signupId").value;
-            password = document.getElementById("signupPw").value;
-            nickname = document.getElementById("signupNickname").value;
-            email = document.getElementById("signupEmail").value;
-
-            xhr.onreadystatechange = function() {
-                if(this.readyState == 4){
-                    if(this.status == 200){
-                        alert("Success! " + this.responseText);
-                    }else if(this.status == 400){
-                        alert("Fail! " + this.responseText);
-                    }else{
-                        alert(this.status + this.responseText);
-                    }
-                }
-            };
-            params = "username=" + username + "&password=" + password + "&nickname=" + nickname + "&email=" + email;
-            
-            console.log(params);
-            
-            xhr.open("POST", "http://lapio.kr/api/register.php", true);
-            
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            
-            xhr.send(params);
-
-            return false;
-        }
-    }
+//    let inputFile = document.querySelectorAll("input[type='file']");
+//    let imgCheck = document.getElementsByClassName("imgCheck");
+//    for(let i =0; i<inputFile.length; i++) {
+//        inputFile[i].onchange = function() {
+//            imgCheck[i].style.background = "url("+");";
+//        }
+//    }
 };
