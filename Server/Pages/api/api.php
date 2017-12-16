e40cfc1d3bcc69b1406c3151226739cad735efa5
SHGroup <sunghun7511@naver.com>
2017-12-17 01:36:39 +0900

    modify some api.php

--- Server/Pages/api/api.php
+++ Server/Pages/api/api.php
@@ -1,5 +1,10 @@
 <?php
     include_once("./db.php");
+
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("charset=utf-8");
+
     function hash_password($password) {
         return hash('sha512', $password);
     }
@@ -11,20 +16,4 @@
     function getUsernameFromAccessToken($access_token){
         return "username";
     }
-
-
-    function login($username, $password) {
-        $password = hash_password($password);
-
-        $query = "select * from User where username='{$username}' and phash='{$password}";
-        $result = mysqli_query($sql_conn, $query);
-        $rows = mysqli_fetch_array($result);
-        if(isset($rows['username'])) {
-            return "";
-        } else {
-            return FALSE;
-        }
-    }
-
-    
 ?>
